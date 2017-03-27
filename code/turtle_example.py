905935b9bcc45ff8826d07535c283a6197f880a4
KStpierre <kevinsporty1@gmail.com>
2017-03-27 10:27:25 -0400

    Add files via upload

---
+++ code/turtle_example.py
@@ -0,0 +1,24 @@
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    tom = turtle.Turtle()
+    tom.forward(200)
+    tom.right(200)
+    tom.forward(200)
+    tom.right(200)
+    tom.forward(200)
+    tom.right(200) 
+    
+    window.exitonclick()    
+
+
+draw_square() 
+   
+    
+
+    
+ 
+    
