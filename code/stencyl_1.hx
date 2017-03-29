87010b2d2acdc4e5c9e2cfa50a02af682822e04a
KStpierre <kevinsporty1@gmail.com>
2017-03-29 10:35:51 -0400

    Create stencyl_1.hx

---
+++ code/stencyl_1.hx
@@ -0,0 +1,16 @@
+/* ======================== When Updating ========================= */
+addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
+{
+	if(wrapper.enabled)
+	{
+		actor.setXVelocity(0);
+		if(isKeyDown("right"))
+		{
+			actor.setXVelocity(20);
+		}
+		else if(isKeyDown("left"))
+		{
+			actor.setXVelocity(-20);
+		}
+	}
+});
